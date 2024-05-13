c687fd40bc8873cbf3a6978906547d0e163c45d9
sivaperumal64 <164004904+sivaperumal64@users.noreply.github.com>
2024-05-13 14:31:55 +0530

    mapfill.js

--- map and filter web page/mapfill.js
+++ map and filter web page/mapfill.js
@@ -1,45 +1,38 @@
-// Sample movie data
+
 const movies = [
-  { title: "The Hangover", genre: "comedy" },
-  { title: "Scary Movie", genre: "comedy" },
-  { title: "The Conjuring", genre: "horror" },
-  { title: "Get Out", genre: "horror" },
-  { title: "Pulp Fiction", genre: "crime" }
+    { title: "The Shawshank Redemption", genre: "drama" },
+    { title: "Forest Gump", genre: "drama" },
+    { title: "The Dark Knight", genre: "action" },
+    { title: "Inception", genre: "action" },
+    { title: "The Hangover", genre: "comedy" },
+    { title: "Superbad", genre: "comedy" }
 ];
 
-// Function to display movies for a specific genre
-function displayMovies(genre, movies) {
-  const moviesContainer = document.getElementById(`${genre}-movies`);
-  moviesContainer.innerHTML = '';
-  movies.forEach(movie => {
-      const movieElement = document.createElement('div');
-      movieElement.classList.add('movie');
-      movieElement.textContent = movie.title;
-      moviesContainer.appendChild(movieElement);
-  });
+function filterMovies() {
+    const genre = document.getElementById("genreSelector").value;
+
+
+    const filteredMovies = genre === "all" ? movies : movies.filter(movie => movie.genre === genre);
+
+    displayMovies(filteredMovies);
 }
 
-// Function to filter movies by genre
-function filterMovies(genre) {
-  if (genre === 'all') {
-      return movies;
-  } else {
-      return movies.filter(movie => movie.genre === genre);
-  }
+
+function displayMovies(movies) {
+    const moviesContainer = document.getElementById("moviesContainer");
+    moviesContainer.innerHTML = "";
+
+
+    movies.map(movie => {
+        const movieDiv = document.createElement("div");
+        movieDiv.className = "box";
+        movieDiv.innerHTML = `
+            <h3>${movie.title}</h3>
+            Genre: ${movie.genre.charAt(0).toUpperCase() + movie.genre.slice(1)}
+        `;
+        moviesContainer.appendChild(movieDiv);
+    });
 }
 
-// Initial display of all movies
-displayMovies('comedy', movies.filter(movie => movie.genre === 'comedy'));
-displayMovies('horror', movies.filter(movie => movie.genre === 'horror'));
 
-// Event listener for genre selection
-document.getElementById('genre').addEventListener('change', function() {
-  const selectedGenre = this.value;
-  const filteredMovies = filterMovies(selectedGenre);
-  if (selectedGenre === 'all') {
-      displayMovies('comedy', movies.filter(movie => movie.genre === 'comedy'));
-      displayMovies('horror', movies.filter(movie => movie.genre === 'horror'));
-  } else {
-      displayMovies(selectedGenre, filteredMovies);
-  }
-});
+filterMovies();
